269e1deb7e3d3c803f865c8781ccbd454780c9ad
Shivansh Tariyal <shivanshtariyal@gmail.com>
2023-03-20 11:35:04 +0530

    demo on recyclerview

---
+++ app/src/main/java/com/example/viewbindingrv/TaskList.kt
@@ -0,0 +1,16 @@
+package com.example.viewbindingrv
+
+object TaskList {
+    val taskList=listOf<Task>(
+        Task("Take a walk", timeStamp = "7:00am")
+            ,        Task("Eat Breakfast", timeStamp = "8:00am")
+,        Task("Attend a meeting", timeStamp = "9:00am")
+,        Task("Read a book", timeStamp = "12:00pm")
+,        Task("Eat lunch", timeStamp = "2:00pm")
+,        Task("Buy grocries", timeStamp = "4:00pm")
+,        Task("Watch youtube", timeStamp = "6:00pm")
+,        Task("Eat dinner", timeStamp = "8:00pm")
+,        Task("Go to bed", timeStamp = "10:00pm")
+
+    )
+}
